e5f4d15aaa93f0f4b8b4566327fe1a800724e70b
andreswang0703 <andreswang0703@gmail.com>
2024-07-06 14:05:48 -0400

    #134 update explanation for gas station problem

--- 0134-gas-station/solution.go
+++ 0134-gas-station/solution.go
@@ -2,10 +2,16 @@ package _134_gas_station
 
 import "math"
 
-// Iterate the list and calculate the cumulative surplus, find the point with
-// the lowest cumulative surplus. The next point would be the starting point because
-// if there's guaranteed to be a unique answer, then the step which leads to the lowest point
-// should be the last step, so that all potential increase in surplus have already been included (greedy).
+// The solution is finding the lowest accumulative surplus point, and choose the index after that.
+// The logic behind this solution is:
+//  1. If there is a solution, the cumulative surplus of all points is guaranteed to be non-negative.
+//  2. For choosing a starting point k, we need to choose one that the cumulative surplus of [k..n-1] is bigger
+//     than [0..k-1], so that the surplus of [k..n-1] can cover the expense of [0..k-1].
+//  3. Points that satisfy the previous rule don't automatically guarantee the non-existence of points that might dip
+//     below 0. However, when condition 1 is satisfied, we can choose the point after the lowest cumulative surplus
+//     because firstly it guarantees 2, and secondly it guarantees the non-existence of points that dips below 0 after k,
+//     and that's because if there were, that point would be the actual lowest point, it also guarantees no point would dip below 0
+//     before k, because the cumulative surplus of [k..n-1] is sufficient to cover all cost before k.
 func canCompleteCircuit(gas []int, cost []int) int {
 	minTotal := math.MaxInt8
 	total := 0
